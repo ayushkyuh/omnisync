6535aa29f225683b39fb64e8361b5f08a7cf7652
ayushkyuh <saritasaroha120@gmail.com>
2026-02-08 17:02:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const API = "https://your-backend.onrender.com";
+
+function loadFridge() {
+  fetch(API + "/fridge")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").textContent =
+        JSON.stringify(data, null, 2);
+    });
+}
+
+function loadEnergy() {
+  fetch(API + "/energy")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").textContent =
+        JSON.stringify(data, null, 2);
+    });
+}
+
+function loadCommute() {
+  fetch(API + "/commute")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").textContent =
+        JSON.stringify(data, null, 2);
+    });
+}
